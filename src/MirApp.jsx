7d57971fcca273fbcda560488f0588980f9a84a1
Diego <diegom.gzlz@gmail.com>
2023-11-01 12:17:35 -0600

    Se cambió la estructura del proyecto para trabajar con Vite el desarrollo

---
+++ src/MirApp.jsx
@@ -0,0 +1,15 @@
+
+export const MirApp = ({title, subTitle}) => {
+  
+
+  return (
+    <>
+    <h1>{title}</h1>
+    {/* <code>{JSON.stringify(newMessage)}</code> esta sentencia es para poder imprimir objetos*/}
+    <p>{subTitle + 1}</p>
+    </>
+    
+  );
+}
+
+
